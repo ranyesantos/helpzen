b98947ce5424eb846ce402933f80cb34b65ce29e
ranyesantos <lucasgts17@gmail.com>
2025-08-03 19:18:58 -0300

    feat: user-serviceRequest relation manager

---
+++ app/Filament/Resources/Users/RelationManagers/ServiceRequestsRelationManager.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Filament\Resources\Users\RelationManagers;
+
+use App\Enums\ServiceRequestStatusType;
+use Filament\Actions\AssociateAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DissociateAction;
+use Filament\Actions\DissociateBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class ServiceRequestsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'serviceRequests';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required(),
+                Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                Select::make('device_id')
+                    ->relationship('device', 'id')
+                    ->required(),
+                Select::make('status')
+                    ->options(ServiceRequestStatusType::class)
+                    ->default('pending')
+                    ->required(),
+            ]);
+    }
+
+    public function infolist(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('created_at')
+                    ->dateTime(),
+                TextEntry::make('updated_at')
+                    ->dateTime(),
+                TextEntry::make('title'),
+                TextEntry::make('device.id')
+                    ->numeric(),
+                TextEntry::make('status'),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('title')
+            ->columns([
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('title')
+                    ->searchable(),
+                TextColumn::make('device.id')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('status'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                CreateAction::make(),
+                AssociateAction::make(),
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+                DissociateAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DissociateBulkAction::make(),
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
